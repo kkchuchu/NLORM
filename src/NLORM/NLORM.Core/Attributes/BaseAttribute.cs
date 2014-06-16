3f9570c8311ea5346e8aa320bf3913db2c4c038d
eternnoir <eternnoir@gmail.com>
2014-06-16 10:55:24 +0800

    add baseAttribute class

---
+++ src/NLORM/NLORM.Core/Attributes/BaseAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLORM.Core.Attributes
+{
+    class BaseAttribute : Attribute
+    {
+        protected readonly Guid guId;
+    }
+}
