5b8397f1c33ad7dbfb932cdbd105afb79d0ec1a8
eternnoir <eternnoir@gmail.com>
2014-06-16 19:06:01 +0800

    remove unuse class

--- src/NLORM/NLORM.Core/BasicDefinitions/ColumnFieldDefinition.cs
+++ src/NLORM/NLORM.Core/BasicDefinitions/ColumnFieldDefinition.cs
@@ -8,6 +8,10 @@ namespace NLORM.Core.BasicDefinitions
     public class ColumnFieldDefinition
     {
         public string ColumnName { get; set; }
-        public ColumnTypeDefinition ColumnType { get; set; }
+        public Type FieldType { get; set; }
+        public string Length { get; set; }
+        public bool Nullable { get; set; }
+        public Object DefaultValue { get; set; }
+        public string Comment { get; set; }
     }
 }
