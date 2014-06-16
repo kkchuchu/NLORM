ec4ca79dd1bc94915ec242b6ed8ef1be8afbd99b
eternnoir <eternnoir@gmail.com>
2014-06-16 19:03:37 +0800

    refactor

--- src/NLORM/NLORM.Core/BasicDefinitions/ModelDefinition.cs
+++ src/NLORM/NLORM.Core/BasicDefinitions/ModelDefinition.cs
@@ -8,12 +8,13 @@ namespace NLORM.Core.BasicDefinitions
     public class ModelDefinition
     {
         public string TableName { get { return this.tableName; } }
-        public Dictionary<string, ColumnFieldDefinition> PropertyColumnDic;
+        public Dictionary<string, ColumnFieldDefinition> PropertyColumnDic{get{return this.propertyColumnDic;}}
+        private Dictionary<string, ColumnFieldDefinition> propertyColumnDic;
         private string tableName;
-        public ModelDefinition(string tablename, Dictionary<string, ColumnFieldDefinition> propertyColumnDic)
+        public ModelDefinition(string tablename, Dictionary<string, ColumnFieldDefinition> propertycolumnDic)
         {
             tableName = tablename;
-            PropertyColumnDic = propertyColumnDic;
+            propertyColumnDic = propertycolumnDic;
         }
         
     }
