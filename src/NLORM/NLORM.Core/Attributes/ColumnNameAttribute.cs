a58e0fcbd05b5f3613793d08312e16dbb472a6dc
eternnoir <eternnoir@gmail.com>
2014-06-16 10:56:00 +0800

    add ColumnNameAttribute

---
+++ src/NLORM/NLORM.Core/Attributes/ColumnNameAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLORM.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class ColumnNameAttribute : BaseAttribute
+    {
+        public ColumnNameAttribute(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+        public string ColumnName { get; set; }
+    }
+ }
