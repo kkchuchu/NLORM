f2d712a0361fa2368506be812abccc105d55282e
eternnoir <eternnoir@gmail.com>
2014-06-17 15:54:56 +0800

    add Sqliteconnection

---
+++ src/NLORM/NLORM.SQLite/SQLiteConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLORM.Core;
+
+namespace NLORM.SQLite
+{
+    public class SQLiteConnection : INLORMDbConnection
+    {
+
+        public bool CreateTable<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
