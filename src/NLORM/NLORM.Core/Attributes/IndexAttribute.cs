487e0cac11033739d0e42c34631d67e01847764b
Yi-Chung Chen <kkchuchualbert@gmail.com>
2014-06-20 01:03:10 -0700

    Add IndexAttribute

---
+++ src/NLORM/NLORM.Core/Attributes/IndexAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLORM.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class IndexAttribute : BaseAttribute
+	{
+		public IndexAttribute()
+		{
+			this.YesorNo = true;
+		}
+		public bool YesorNo { get; set;}
+	}
+}
