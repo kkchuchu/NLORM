04603e10f258113d9c395670941ff716e26323b6
eternnoir <eternnoir@gmail.com>
2014-06-21 20:28:03 +0800

    add two interface

---
+++ src/NLORM/NLORM.Core/ISqlGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace NLORM.Core
+{
+    public interface ISqlGenerator
+    {
+        string GenCreateTableSqlByDbType(DbType type, string length);
+    }
+}
