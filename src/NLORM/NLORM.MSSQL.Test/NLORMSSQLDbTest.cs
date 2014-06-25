a34d2dd64f76a280e86a26f9c47c06d72987cb83
Yi-Chung Chen <kkchuchualbert@gmail.com>
2014-06-24 22:25:10 -0700

    add test mssql test case, all mssql tests passed

--- src/NLORM/NLORM.MSSQL.Test/NLORMSSQLDbTest.cs
+++ src/NLORM/NLORM.MSSQL.Test/NLORMSSQLDbTest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLORM.Core;
 using NLORM.Core.Attributes;
+using System;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace NLORM.MSSQL.Test
 {
@@ -11,14 +13,90 @@ namespace NLORM.MSSQL.Test
         public string ID { get; set; }
     }
 
+	public class APPLY
+	{
+		string APPLICATION_ID;
+		string SEQ_NO;
+		string STEP;
+		DateTime CREATE_DT;
+		string CREATE_USER;	
+		string URGENT_FLAG;	
+		string DATA_XML;//XML	
+		string STATUS;
+		string STATUS_STEP;	
+		string REJECT_GROUP;
+		string REJECT_CODE;	
+		DateTime RETECT_DTE;	
+		string APPLY_USER;	
+		string POST_RESULT;	
+		DateTime MNT_DT;
+		string MNT_USER;
+	}
+
 	[TestClass]
 	public class NLORMSSQLDbTest
     {
+		private string constr = @"Data Source=192.168.95.106;Initial Catalog=CARD_ALBERT;User ID=CyberCArd;Password=Passw0rd";
+
+		[TestInitialize()]
+		public void TestInitialize()
+		{
+			INLORMDb mssqlDb = null;
+			try
+			{
+				mssqlDb = new NLORMMSSQLDb( constr);
+				mssqlDb.DropTable<TestClass01>();
+			}
+			catch ( Exception)
+			{
+			}
+		}
+
+		[TestCleanup()]
+		public void TestCleanup()
+		{
+			INLORMDb mssqlDb = null;
+			try
+			{
+				mssqlDb = new NLORMMSSQLDb( constr);
+				mssqlDb.DropTable<TestClass01>();
+			}
+			catch ( Exception)
+			{
+			}
+		}
+
 		[TestMethod]
         public void TestCreateTable()
         {
-            //NLORMBaseDb mssqlDb = new NLORMMSSQLDb("Data Source=C:\\test.sqlite");
-            //mssqlDb.CreateTable<TestClass01>();
+			INLORMDb mssqlDb = null;
+			try
+			{
+				mssqlDb = new NLORMMSSQLDb( constr);
+				mssqlDb.CreateTable<TestClass01>();				
+			}
+			catch ( SqlException)
+			{
+				Assert.Fail();
+			}
         }
+
+		[TestMethod]
+		public void TestDropTable()
+		{
+			INLORMDb db = null;
+			db = new NLORMMSSQLDb( constr);
+			db.CreateTable<TestClass01>();
+			db.DropTable<TestClass01>();
+		}
+
+		[TestMethod]
+		public void TestQuery()
+		{
+			INLORMDb db = null;
+			db = new NLORMMSSQLDb( constr);
+			db.CreateTable<TestClass01>();
+			db.Insert<TestClass01>( new TestClass01(){ ID = "001"} );
+		}
 	}
 }
