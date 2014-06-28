d04f86aff1d6e003683998d37e92cba55b9ab56c
eternnoir <eternnoir@gmail.com>
2014-06-28 18:30:26 +0800

    int support and add new testcase

---
+++ src/NLORM/NLORM.SQLite.Test/TestCreateTable.cs
@@ -0,0 +1,120 @@
+﻿using System.Data;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLORM.Core.Attributes;
+
+namespace NLORM.SQLite.Test
+{
+    /// <summary>
+    /// Summary description for TestCreateTable
+    /// </summary>
+    [TestClass]
+    public class TestCreateTable
+    {
+        string connectionString;
+        private static string filePath;
+        public TestCreateTable()
+        {
+            filePath = "C:\\test.sqlite";
+            connectionString = "Data Source=" + filePath;
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            finally
+            {
+
+            }
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            finally
+            {
+
+            }
+        }
+
+        class TestClassOnlyString
+        {
+            public string ID { get; set; }
+        }
+
+        [TestMethod]
+        public void TestCreateTableOnlyString()
+        {
+            var sqliteDbc = new NLORMSQLiteDb(connectionString);
+            sqliteDbc.CreateTable<TestClassOnlyString>();
+        }
+
+        class TestClassOnlyStringWCfd
+        {
+            [ColumnType(DbType.String, "30", false, "this is id comment")]
+            public string ID { get; set; }
+        }
+
+
+        [TestMethod]
+        public void TestCreateTableOnlyStringWithCfd()
+        {
+            var sqliteDbc = new NLORMSQLiteDb(connectionString);
+            sqliteDbc.CreateTable<TestClassOnlyStringWCfd>();
+        }
+
+        class TestClassOnlyInt
+        {
+            public int ID { get; set; }
+        }
+
+        [TestMethod]
+        public void TestCreateTableOnlyInt()
+        {
+            var sqliteDbc = new NLORMSQLiteDb(connectionString);
+            sqliteDbc.CreateTable<TestClassOnlyInt>();
+        }
+
+        class TestClassOnlyIntWCfd
+        {
+            [ColumnType(DbType.Int32, "", false, "this is id comment")]
+            public int ID { get; set; }
+
+            [ColumnType(DbType.Int16, "", false, "this is id comment")]
+            public int ID2 { get; set; }
+        }
+
+        [TestMethod]
+        public void TestCreateTableOnlyIntWcfd()
+        {
+            var sqliteDbc = new NLORMSQLiteDb(connectionString);
+            sqliteDbc.CreateTable<TestClassOnlyIntWCfd>();
+        }
+    }
+}
