7e428cf1e391e9f887b594bcce98cd5fee784813
kkchuchu <albertchenyichung@gmail.com>
2014-06-28 09:50:03 -0700

    add NLAdapter Test Case in SQLite.Test

--- src/NLORM/NLORM.Core/NLAdapter.cs
+++ src/NLORM/NLORM.Core/NLAdapter.cs
@@ -12,26 +12,67 @@ namespace NLORM.Core
 		private INLORMDb db;
 		private string selectstr;
 		private IEnumerable<T> items;
+		private NLCollection<T> collection;
 		private int count;
-		private List<string> script;
 		private ISqlBuilder sqlBuilder;
 		public NLAdapter( INLORMDb db, string selectstring)
 		{
 			this.db = db;
 			this.selectstr = selectstring;
-
-			items = db.Query<T>(selectstr);
+			this.items = db.Query<T>(selectstr);
+			this.collection.ToNLC( items);
 			this.count = items.Count();
 		}
 
+		public NLCollection<T> Collection
+		{
+			get
+			{
+				return collection;
+			}
+		}
+	}
+
+	public class NLCollection<T> : IEnumerable, IEnumerable<T>
+	{
+		public int Count 
+		{ 
+			get
+			{
+				return this.List.Count;
+			} 
+		}
+		protected ArrayList List 
+		{ 
+			get
+			{
+				return List;
+			} 
+		}
+		private List< Dapper.CommandDefinition> multiscripts;
+		public virtual void CopyTo(Array ar, int index)
+		{
+		}
+		public void  ToNLC( IEnumerable<T> coll)
+		{
+			this.List.AddRange( coll as ICollection);
+		}
+
+		public T this[ int index]
+		{
+			get
+			{
+				List.Add( new Dapper.CommandDefinition( @""));
+				return (T)List[index];
+			}
+		}
 		public int Add( T item)
 		{
-			items = items.Concat( new[] {item});
+			List.Add( item);
 
-			script.Add( sqlBuilder.GenInsertSql<T>());
-			count++;
+			List.Add( new Dapper.CommandDefinition( @""));
 
-			return count;
+			return this.Count;
 		}
 
 		public bool Remove(T item)
@@ -39,8 +80,8 @@ namespace NLORM.Core
 			bool succedtag = true;
 			try
 			{
-				items = items.Except( new[] {item});
-				script.Add( "");// todo
+				List.Remove( item);
+				List.Add( new Dapper.CommandDefinition( @""));// todo
 			}
 			catch ( ArgumentNullException)
 			{
@@ -50,18 +91,72 @@ namespace NLORM.Core
 			return succedtag;
 		}
 
-		public IEnumerable<T> Items
+		IEnumerator IEnumerable.GetEnumerator()
 		{
-			get
-			{
-				return items;
-			}
+		   return (IEnumerator) GetEnumerator();
+		}
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return (IEnumerator<T>) GetEnumerator();
+		}
+
+		public NLCollectionEnum<T> GetEnumerator()
+		{
+			return new NLCollectionEnum<T>(List.ToArray() as T[]);
 		}
 	}
 
-	public class NLItem<T> : IEnumerable<T>, ICollection, IEnumerable
+	public class NLCollectionEnum<T> : IEnumerator, IEnumerator<T>
 	{
-		
-	}
+		public T[] _collect;
 
+		// Enumerators are positioned before the first element
+		// until the first MoveNext() call.
+		int position = -1;
+
+		public NLCollectionEnum(T[] list)
+		{
+			_collect = list;
+		}
+
+		public bool MoveNext()
+		{
+			position++;
+			return (position < _collect.Length);
+		}
+
+		public void Reset()
+		{
+			position = -1;
+		}
+
+		object IEnumerator.Current
+		{
+			get
+			{
+				return Current;
+			}
+		}
+
+		public T Current
+		{
+			get
+			{
+				try
+				{
+					return _collect[position];
+				}
+				catch (IndexOutOfRangeException)
+				{
+					throw new InvalidOperationException();
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			this._collect = null;
+		}
+	}
 }
---
+++ src/NLORM/NLORM.SQLite.Test/NLAdapterTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLORM.SQLite;
+using NLORM.Core;
+using NLORM.Core.Attributes;
+using NLORM.Core.BasicDefinitions;
+using System.IO;
+
+
+namespace NLORM.SQLite.Test
+{
+	class Testclass01
+	{
+		public string ID { get; set;}
+	}
+	[TestClass]
+	public class NLAdapterTest
+	{
+		private string connectionString;
+		public NLAdapterTest()
+		{
+			string filePath = "C:\\test.sqlite";
+            connectionString = "Data Source="+filePath;
+		}
+		[TestMethod]
+		public void TestCallNLapter()
+		{
+			INLORMDb db = new SQLite.NLORMSQLiteDb( connectionString);
+			string selectstr = @"SELECT * FROM Testclass01";
+			NLORM.Core.NLAdapter<Testclass01> adapter = new NLORM.Core.NLAdapter<Testclass01>( db, selectstr);
+			adapter.Collection.Add( new Testclass01(){ ID = @"10"} );
+			adapter.Collection.Add( new Testclass01(){ ID = @"20"} );// if add same items, what's the result.
+			adapter.Collection.Remove( new Testclass01(){ ID = @"10"});
+			adapter.Collection[0].ID = @"4";
+
+			foreach ( Testclass01 item in adapter.Collection)
+			{
+				
+			}
+		}
+	}
+}
