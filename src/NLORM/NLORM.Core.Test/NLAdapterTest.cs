8a32af663ee9faf5263b692c581fd6fff4cb424b
kkchuchu <albertchenyichung@gmail.com>
2014-06-27 20:22:46 -0700

    adapter adds methods : add, remove
    i think i need imp remove sqlstringbuilder

---
+++ src/NLORM/NLORM.Core.Test/NLAdapterTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLORM.Core.Test
+{
+	class Testclass01
+	{
+		public string ID { get; set;}
+	}
+	public class NLAdapterTest
+	{
+		[TestMethod]
+		public void TestCallNLapter()
+		{
+			INLORMDb db = new NLORMBaseDb();
+			string selectstr = @"SELECT * FROM ";
+			NLAdapter<Testclass01> adapter = new NLAdapter<Testclass01>( db, selectstr);
+//			adapter
+		}
+	}
+}
---
+++ src/NLORM/NLORM.Core/NLAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace NLORM.Core
+{
+	public class NLAdapter<T>
+	{		
+		private INLORMDb db;
+		private string selectstr;
+		private IEnumerable<T> items;
+		private int count;
+		private List<string> script;
+		private ISqlBuilder sqlBuilder;
+		public NLAdapter( INLORMDb db, string selectstring)
+		{
+			this.db = db;
+			this.selectstr = selectstring;
+
+			items = db.Query<T>(selectstr);
+			this.count = items.Count();
+		}
+
+		public int Add( T item)
+		{
+			items = items.Concat( new[] {item});
+
+			script.Add( sqlBuilder.GenInsertSql<T>());
+			count++;
+
+			return count;
+		}
+
+		public bool Remove(T item)
+		{
+			bool succedtag = true;
+			try
+			{
+				items = items.Except( new[] {item});
+				script.Add( "");// todo
+			}
+			catch ( ArgumentNullException)
+			{
+				succedtag = false;
+			}
+
+			return succedtag;
+		}
+
+		public IEnumerable<T> Items
+		{
+			get
+			{
+				return items;
+			}
+		}
+	}
+
+}
