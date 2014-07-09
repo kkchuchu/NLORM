771f14ef465d8cf4592a6d447286360a49babc05
eternnoir <eternnoir@gmail.com>
2014-07-09 17:29:43 +0800

    Add Update test case

---
+++ src/NLORM/NLORM.SQLite.Test/TestUpdate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLORM.Core;
+
+
+namespace NLORM.SQLite.Test
+{
+    /// <summary>
+    /// Summary description for TestUpdate
+    /// </summary>
+    [TestClass]
+    public class TestUpdate
+    {
+        string connectionString;
+        private static string filePath;
+        public TestUpdate()
+        {
+            filePath = "C:\\test.sqlite";
+            connectionString = "Data Source=" + filePath;
+        }
+
+        class TestClassOne
+        {
+            public string Id { get; set;}
+
+            public int income { get; set;}
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            try
+            {
+                File.Delete(filePath);
+                var db = this.createtable();
+                this.insertdata( db);
+            }
+            finally
+            {
+
+            }
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            finally
+            {
+
+            }
+        }
+        private INLORMDb createtable()
+        {
+            var db = new NLORMSQLiteDb(connectionString);
+            db.CreateTable<TestClassOne>();
+            return db;
+        }
+        private void insertdata(INLORMDb db)
+        {
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "sssss", income = 123456 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "rrrrr", income = 789012 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "fffff", income = 345678 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "lllll", income = 901234 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "alber", income = 901234 });
+        }
+
+
+
+        [TestMethod]
+        public void TestUpdateOneRow()
+        {
+            var db = new NLORMSQLiteDb(connectionString);
+            var newobj = new TestClassOne { Id = "sssss", income = 100 };
+            int i = db.FliterBy(FliterType.EQUAL_AND, new { Id = "sssss" }).Update<TestClassOne>(newobj);
+            var items = db.FliterBy(FliterType.EQUAL_AND, new { Id = "sssss" }).Query<TestClassOne>().First();
+            Assert.AreEqual(100, items.income);
+        }
+    }
+}
