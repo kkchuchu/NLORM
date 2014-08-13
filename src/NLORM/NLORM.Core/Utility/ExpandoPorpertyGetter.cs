023aaf836fcefb00b9fcf15330cf7c56637f4fdb
eternnoir <eternnoir@gmail.com>
2014-08-13 12:26:28 +0800

    add porperty getter

---
+++ src/NLORM/NLORM.Core/Utility/ExpandoPorpertyGetter.cs
@@ -0,0 +1,20 @@
+﻿using NLORM.Core.BasicDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace NLORM.Core.Utility
+{
+    public class ExpandoPorpertyGetter : IPropertyGetter
+    {
+        public IDictionary<string, object> GetPropertyDic(FilterObject Fo)
+        {
+            var eoDic = Fo.Cons as IDictionary<string,object>;
+            Debug.Assert(eoDic != null);
+            return eoDic;            
+        }
+    }
+}
