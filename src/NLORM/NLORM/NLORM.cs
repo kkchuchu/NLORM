495d27d6fe363c62deb748fde709d2d0280a9486
eternnoir <eternnoir@gmail.com>
2014-08-16 22:07:20 +0800

    add nlorm Factory

---
+++ src/NLORM/NLORM/NLORM.cs
@@ -0,0 +1,16 @@
+﻿using NLORM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLORM
+{
+    public static class NLORM
+    {
+        public static INLORMDb GetDb()
+        {
+            return null;
+        }
+    }
+}
