07b88ac4ae6d184107fc0a8d96a47c8fefecc26c
eternnoir <eternnoir@gmail.com>
2014-09-26 16:01:11 +0800

    add delete test

--- src/NLORM/NLORM.MySql.Test/TestDelete.cs
+++ src/NLORM/NLORM.MySql.Test/TestDelete.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLORM.Core;
 
 namespace NLORM.MySql.Test
 {
@@ -37,34 +38,72 @@ namespace NLORM.MySql.Test
             }
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+        class TestClassOne
+        {
+            public string Id { get; set; }
+
+            public int income { get; set; }
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            var dbc = new NLORMMySqlDb(connectionString);
+            createtable(dbc);
+            insertdata(dbc);
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+        }
+
+
+        private void createtable(INLORMDb db)
+        {
+            try
+            {
+                db.DropTable<TestClassOne>();
+            }
+            catch { }
+            db.CreateTable<TestClassOne>();
+        }
+        private void insertdata(INLORMDb db)
+        {
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "sssss", income = 123456 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "rrrrr", income = 789012 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "fffff", income = 345678 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "lllll", income = 901234 });
+            db.Insert<TestClassOne>(new TestClassOne() { Id = "alber", income = 901234 });
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestDeleteOneRecord()
         {
-            //
-            // TODO: Add test logic here
-            //
+            var db = new NLORMMySqlDb(connectionString);
+            int i = db.FilterBy(FilterType.EQUAL_AND, new { Id = "sssss" }).Delete<TestClassOne>();
+            var items = db.FilterBy(FilterType.EQUAL_AND, new { Id = "sssss" }).Query<TestClassOne>();
+            Assert.AreEqual(0, items.Count());
+        }
+
+        [TestMethod]
+        public void TestDeleteTwoRecord()
+        {
+            var db = new NLORMMySqlDb(connectionString);
+            db.FilterBy(FilterType.EQUAL_AND, new { income = 901234 }).Delete<TestClassOne>();
+            var items = db.Query<TestClassOne>();
+            Assert.AreEqual(3, items.Count());
+        }
+
+        [TestMethod]
+        public void TestDeleteAllRecords()
+        {
+            var db = new NLORMMySqlDb(connectionString);
+            int totalcount = db.Query<TestClassOne>().Count();
+            int dc = db.Delete<TestClassOne>();
+
+            var items = db.Query<TestClassOne>();
+            Assert.AreEqual(dc, totalcount);
         }
     }
 }
