d331820968fcfa1bd6a5c9c1e3c201079c5c1fff
eternnoir <eternnoir@gmail.com>
2014-09-26 16:42:37 +0800

    add transaction test

--- src/NLORM/NLORM.MySql.Test/TestTransaction.cs
+++ src/NLORM/NLORM.MySql.Test/TestTransaction.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLORM.Core;
 
 namespace NLORM.MySql.Test
 {
@@ -36,34 +37,136 @@ namespace NLORM.MySql.Test
             }
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            var dbc = new NLORMMySqlDb(connectionString);
+            Init(dbc);
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            try
+            {
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void Init(INLORMDb db)
+        {
+            try
+            {
+                db.DropTable<TestClassUser>();
+            }
+            catch { }
+            db.CreateTable<TestClassUser>();
+        }
+
+
+
+        private class TestClassUser
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public DateTime CreateTime { get; set; }
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestTransactionCommit()
         {
-            //
-            // TODO: Add test logic here
-            //
+            var mySqlDbc = new NLORMMySqlDb(connectionString);
+            var trans = mySqlDbc.BeginTransaction();
+            var testObj = new TestClassUser();
+            testObj.ID = 1;
+            testObj.Name = "Name " + 1;
+            testObj.CreateTime = DateTime.Now;
+            mySqlDbc.Insert<TestClassUser>(testObj);
+            trans.Commit();
+            mySqlDbc.Close();
+            mySqlDbc = new NLORMMySqlDb(connectionString);
+            var selLis = mySqlDbc.Query<TestClassUser>();
+            Assert.AreEqual(selLis.Count(), 1);
+
+        }
+
+        [TestMethod]
+        public void TestTransactionRollback()
+        {
+            var mySqlDbc = new NLORMMySqlDb(connectionString);
+            var trans = mySqlDbc.BeginTransaction();
+            var testObj = new TestClassUser();
+            testObj.ID = 1;
+            testObj.Name = "Name " + 1;
+            testObj.CreateTime = DateTime.Now;
+            mySqlDbc.Insert<TestClassUser>(testObj);
+            trans.Rollback();
+            mySqlDbc.Close();
+            mySqlDbc = new NLORMMySqlDb(connectionString);
+            var selLis = mySqlDbc.Query<TestClassUser>();
+            Assert.AreEqual(selLis.Count(), 0);
+
+        }
+
+
+        [TestMethod]
+        public void TestInsertUserClassMutiCommit()
+        {
+            var mySqlDbc = new NLORMMySqlDb(connectionString);
+            var trans = mySqlDbc.BeginTransaction();
+            var insertList = new List<TestClassUser>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var testObj = new TestClassUser
+                {
+                    ID = i,
+                    Name = "Name " + i,
+                    CreateTime = DateTime.Now.AddDays(i)
+                };
+                insertList.Add(testObj);
+            }
+            foreach (TestClassUser user in insertList)
+            {
+                mySqlDbc.Insert<TestClassUser>(user);
+            }
+            trans.Commit();
+            mySqlDbc.Close();
+            mySqlDbc = new NLORMMySqlDb(connectionString);
+            var selLis = mySqlDbc.Query<TestClassUser>();
+            Assert.AreEqual(selLis.Count(), 30);
+        }
+
+        [TestMethod]
+        public void TestInsertUserClassMutiRollback()
+        {
+            var mySqlDbc = new NLORMMySqlDb(connectionString);
+            var trans = mySqlDbc.BeginTransaction();
+            var insertList = new List<TestClassUser>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var testObj = new TestClassUser
+                {
+                    ID = i,
+                    Name = "Name " + i,
+                    CreateTime = DateTime.Now.AddDays(i)
+                };
+                insertList.Add(testObj);
+            }
+            foreach (TestClassUser user in insertList)
+            {
+                mySqlDbc.Insert<TestClassUser>(user);
+            }
+            trans.Rollback();
+            mySqlDbc.Close();
+            mySqlDbc = new NLORMMySqlDb(connectionString);
+            var selLis = mySqlDbc.Query<TestClassUser>();
+            Assert.AreEqual(selLis.Count(), 0);
         }
     }
 }
