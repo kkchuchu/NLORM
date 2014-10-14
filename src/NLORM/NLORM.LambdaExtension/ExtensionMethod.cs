989e49347fc4cf435bab8ced08adc4c41735dae5
kkchuchu <albertchenyichung@gmail.com>
2014-10-14 18:34:31 +0800

    edit lambda class

--- src/NLORM/NLORM.LambdaExtension/ExtensionMethod.cs
+++ src/NLORM/NLORM.LambdaExtension/ExtensionMethod.cs
@@ -1,6 +1,7 @@
 ﻿using NLORM.Core;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -35,5 +36,9 @@ namespace NLORM.LambdaExtension
         {
             return 0;
         }
+
+
     }
+
+    
 }
