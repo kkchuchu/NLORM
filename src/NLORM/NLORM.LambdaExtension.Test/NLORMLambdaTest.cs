4355f65415d5bc4a69b0362735369f20708f3053
kkchuchu <albertchenyichung@gmail.com>
2014-10-14 17:41:54 +0800

    Lambda class and test class

---
+++ src/NLORM/NLORM.LambdaExtension.Test/NLORMLambdaTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLORM.LambdaExtension;
+using NLORM.Core;
+using NLORM.MSSQL;
+
+namespace NLORM.LambdaExtension.Test
+{
+    class Model
+    {
+        public string ID { get; set;}
+
+        public string NAME { get; set;}
+    }
+
+    [TestClass]
+    public class NLORMLambdaTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestCallMethd()
+        {
+            INLORMDb db = new NLORMMSSQLDb();
+            db.Where<Model>( x => x.NAME == "albert" ).Select<Model>( x => new { x.ID, x.NAME});
+            
+            db.Insert<Model>( new Model(){ ID = "111", NAME = "azzz"});
+            db.Where<Model>( x => x.ID == "123" && x.ID == "456").Delete<Model>();
+            db.Where<Model>( x => x.ID == "asdf").Update<Model>( new { ID = "123", NAME = "aooqwe"});
+        }
+    }
+}
---
+++ src/NLORM/NLORM.LambdaExtension/ExtensionMethod.cs
@@ -0,0 +1,38 @@
+﻿using NLORM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLORM.LambdaExtension
+{
+    public static class ExtensionMethod
+    {
+        public static INLORMDb Where<T>( this INLORMDb db, Expression<Func<T, bool>> exp)
+        {            
+            return db;
+        }
+
+        public static List<object> Select<T>( this INLORMDb db, Expression<Func<T, object>> exp)
+        {
+            return new List<object>();
+        }
+
+        public static bool Insert<T>( this INLORMDb db, T t)
+        {
+            return false;
+        }
+
+        public static int Update<T>( this INLORMDb db, object t)
+        {
+            return 0;
+        }
+
+        public static int Delete<T>( this INLORMDb db)
+        {
+            return 0;
+        }
+    }
+}
