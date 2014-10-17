e9cfbbe91fcfd6f65495fea17b830e1d4e7a1ed5
kkchuchu <albertchenyichung@gmail.com>
2014-10-17 11:26:35 +0800

    implement LambdaExtensionMethod

--- src/NLORM/NLORM.LambdaExtension/ExtensionMethod.cs
+++ src/NLORM/NLORM.LambdaExtension/ExtensionMethod.cs
@@ -21,4 +21,33 @@ namespace NLORM.LambdaExtension
 
         int Delete<T>();
     }    
+
+    public partial class NLORMBaseDb : INLORMDb
+    {
+        public INLORMDb Where<T>( Expression<Func<T, bool>> exp)
+        {
+            return this;
+        }
+
+        public List<object> Select<T> ( Expression<Func<T, object>> exp)
+        {
+            return new List<object>();
+        }
+
+        public bool Insert<T> ( T t)
+        {
+            return false;
+        }
+
+        public int Update<T>( object t)
+        {
+            return 0;
+        }
+
+        public int Delete<T>()
+        {
+            return 0;
+        }
+    }
+
 }
