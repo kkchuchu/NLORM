810e3d2da605b693154a4c71efc36663b6fb9352
kkchuchu <albertchenyichung@gmail.com>
2014-10-17 16:10:58 +0800

    edit test case

--- src/NLORM/NLORM.LambdaExtension.Test/NLORMLambdaTest.cs
+++ src/NLORM/NLORM.LambdaExtension.Test/NLORMLambdaTest.cs
@@ -37,7 +37,7 @@ namespace NLORM.LambdaExtension.Test
         [TestMethod]
         public void TestCallMethd()
         {
-            NLORMMSSQLDb db = new NLORMMSSQLDb(connectionstring);
+            INLORMDb db = new NLORMMSSQLDb(connectionstring);
             db.Where<Model>( x => x.NAME == "albert" ).Select<Model>( x => new { x.ID, x.NAME});
             
             db.Insert<Model>( new Model(){ ID = "111", NAME = "azzz"});
